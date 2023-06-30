c16f45ad0de9ae4ca5d091953b99bb2ca1ecc520
Igor Ziselman <igor.zc@gmail.com>
2023-06-30 17:59:18 +0300

    added Hebrew Year (Hebrew chars)

--- src/HebrewCalendar.py
+++ src/HebrewCalendar.py
@@ -12,9 +12,13 @@ class HebrewCalendar:
         return today
     def get_hebrew_date_str(self):
         today = self.get_hebrew_date()
+        # get hebrew year from today in hebrew number
+        # today_year = today.year
+        today_year = today.year
+        today_hebrew_year = today.hebrew_year()
         month = hebrewcal.Month(today.year, today.month)
         # curr_month = month.month_name()
         parsha_eng = parshios.getparsha_string(today, israel=True)
         parsha_heb = parshios.getparsha_string(today, israel=True, hebrew=True)
 
-        return f"{today.day}.{today.month} {today.year} ({today.month_name()})\nParsha (Israel)={parsha_eng}({parsha_heb})"
+        return f"{today.day}.{today.month} {today.year} ({today.month_name()}), {today_hebrew_year}\nParsha (Israel)={parsha_eng}({parsha_heb})"
---
+++ tests/demo_HebrewCalendar.py
@@ -0,0 +1,14 @@
+import os
+import sys
+import logging
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'src'))
+import HebrewCalendar as HebrewCalendar
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    hebrew_calendar = HebrewCalendar.HebrewCalendar()
+    hebrew_date_str = hebrew_calendar.get_hebrew_date_str()
+    logging.info(f"Hebrew date: {hebrew_date_str}")
+    hebrew_year_month = hebrew_calendar.hebrew_date_string(True)
+    logging.info(f"Hebrew date: {hebrew_year_month}")
