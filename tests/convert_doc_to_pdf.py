52b0fc39180980556dc69ef499276896a7aba397
Igor Ziselman <igor.zc@gmail.com>
2023-07-28 16:01:25 +0300

    minor

--- tests/convert_doc_to_pdf.py
+++ tests/convert_doc_to_pdf.py
@@ -1,14 +1,31 @@
-import os
-import win32com.client as win32
+# import os
+# import win32com.client as win32
 
-def convert_doc_to_pdf(doc_file_path, pdf_file_path):
-    word_app = win32.gencache.EnsureDispatch("Word.Application")
-    doc = word_app.Documents.Open(doc_file_path)
-    doc.SaveAs(pdf_file_path, FileFormat=17)  # FileFormat=17 specifies PDF format
-    doc.Close()
-    word_app.Quit()
+# #
+# # doc2pdf
+# #
+
+# def convert_doc_to_pdf(doc_file_path, pdf_file_path):
+#     word_app = win32.gencache.EnsureDispatch("Word.Application")
+#     doc = word_app.Documents.Open(doc_file_path)
+#     doc.SaveAs(pdf_file_path, FileFormat=17)  # FileFormat=17 specifies PDF format
+#     doc.Close()
+#     word_app.Quit()
+
+# from docx2pdf import convert
+
+# def doc_to_pdf(doc_path, pdf_path):
+#     convert(doc_path, pdf_path)
+
+import os
+import subprocess
+
+def doc_to_pdf(doc_path, pdf_path):
+    soffice_path = os.path.join(r"C:\Program Files\LibreOffice\program", "soffice.exe")
+    subprocess.run([soffice_path, "--headless", "--convert-to", "pdf", "--outdir", pdf_path, doc_path])
 
 # Usage example
-doc_file_path = ".\data\\סיכום מצגת 4.docx"
-pdf_file_path = ".\@experimtnes\סיכום מצגת 4.docx.pdf"
-convert_doc_to_pdf(doc_file_path, pdf_file_path)
+doc_file_path = ".\data\\test4.docx"
+pdf_file_path = ".\data\\test.pdf"
+# convert_doc_to_pdf(doc_file_path, pdf_file_path)
+doc_to_pdf(doc_file_path, pdf_file_path)
--- tests/demo_HebrewCalendar.py
+++ tests/demo_HebrewCalendar.py
@@ -10,5 +10,6 @@ if __name__ == "__main__":
     hebrew_calendar = HebrewCalendar.HebrewCalendar()
     hebrew_date_str = hebrew_calendar.get_hebrew_date_str()
     logging.info(f"Hebrew date: {hebrew_date_str}")
-    hebrew_year_month = hebrew_calendar.hebrew_date_string(True)
-    logging.info(f"Hebrew date: {hebrew_year_month}")
+    logging.info(f"holiday: {hebrew_calendar.get_holiday()}")
+    # hebrew_year_month = hebrew_calendar.hebrew_date_string(True)
+    # logging.info(f"Hebrew date: {hebrew_year_month}")
